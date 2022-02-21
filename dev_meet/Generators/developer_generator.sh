0861b71f92613dc7f3efdad92134bb6b9ed97070
mariogmez <mariogomex199921@gmail.com>
2022-02-21 18:19:29 +0100

    CMT 18:18 21/02

--- dev_meet/Generators/developer_generator.sh
+++ dev_meet/Generators/developer_generator.sh
@@ -6,7 +6,7 @@ do
     dni=$(echo $line | cut -d',' -f1)
     nombre=$(echo $line | cut -d',' -f2)
     year=$(echo $line | cut -d',' -f3)
-    echo "<record id='$dni' model='school.student'>"
+    echo "<record id='$dni' model='dev_meet.developer'>"
     echo "<field name='name'>$nombre</field>"
     echo "<field name='dni'>$dni</field>"
     echo "<field name='birth_year'>$year</field>"
