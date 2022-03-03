4f51bbcf9b2b8ae17f0ffb9f6eadac1fb30f3c6a
mariogmez <mariogomex199921@gmail.com>
2022-03-03 19:53:28 +0100

    CMT 19:53 03/03

--- dev_meet/__manifest__.py
+++ dev_meet/__manifest__.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "dev_meet",
+    'name': "Developer meet",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Gestiona y simula un sistema de reuniones de desarrolladores""",
 
     'description': """
         Long description of module's purpose
